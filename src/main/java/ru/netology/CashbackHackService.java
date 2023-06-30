f20dcca244b363a9b74f4b0bcd1532354da19a32
Ivan Pozdov <Ivan.pozdov58@gmail.com>
2023-06-30 19:48:25 +0300

    Initial commit

--- src/main/java/ru/netology/CashbackHackService.java
+++ src/main/java/ru/netology/CashbackHackService.java
@@ -1,7 +1,9 @@
-package ru.netology.service;
+package ru.netology;
 
 public class CashbackHackService {
     private final int boundary = 1000;
 
-    public int remain(int amount) {return boundary - amount % boundary; }
+    public int remain(int amount) {
+        return boundary - amount % boundary;
+    }
 }
--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,4 +1,38 @@
-import static org.testng.Assert.*;
+package ru.netology;
+
+import static org.testng.AssertJUnit.*;
+import static org.testng.Assert.assertEquals;
+
+
+
 public class CashbackHackServiceTest {
-    
+
+    @org.testng.annotations.Test
+    public void shouldRemainTheAmount() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldCalculateWith900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldCalculateLessThen1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = service.remain(amount);
+        int expected = 200;
+        assertEquals(actual, expected);
+    }
+
 }
+
